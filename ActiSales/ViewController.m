269bbb07bda9e22624cf12eb6b5a981b83de2270
Noirdemort <darkStillwater@protonmail.com>
2021-10-20 11:15:02 +0530

    map elements from story board and add picker delegates conformance

--- ActiSales/ViewController.m
+++ ActiSales/ViewController.m
@@ -9,6 +9,17 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIPickerView *productPicker;
+
+@property (weak, nonatomic) IBOutlet UILabel *selectedProductLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *maxSalesLabel;
+
+@property (weak, nonatomic) IBOutlet UISwitch *autoUpdateSwitch;
+
+@property (strong, nonatomic) NSDictionary *products;
+@property (strong, nonatomic) NSArray *productNames;
+
 @end
 
 @implementation ViewController
@@ -16,7 +27,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.productPicker.delegate = self;
+    self.productPicker.dataSource = self;
 }
+
+
+- (NSInteger)numberOfComponentsInPickerView:(nonnull UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(nonnull UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return [self.productNames count];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return self.productNames[row];
+}
+
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    self.selectedProductLabel.text = self.productNames[row];
+    
+    if ([self.autoUpdateSwitch isOn]) {
+        [self didTapStatsButton:NULL];
+    }
+}
+
 
 
 @end
