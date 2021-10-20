263ee8bcce132e0f992588af70a780337f88dcd4
Noirdemort <darkStillwater@protonmail.com>
2021-10-20 11:15:36 +0530

    use transformed catalogue to show and update results

--- ActiSales/ViewController.m
+++ ActiSales/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import "Product.h"
 
 @interface ViewController ()
 
@@ -22,14 +23,65 @@
 
 @end
 
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.products = [Product getProductCatalogue];
+    self.productNames = [self.products allKeys];
     
     self.productPicker.delegate = self;
     self.productPicker.dataSource = self;
+}
+
+
+/// Selects the country with highest sales if the product is selected.
+/// @param sender element calling the function
+- (IBAction)didTapStatsButton:(id)sender {
+    
+    if (self.selectedProductLabel.text == NULL) {
+        return;
+    }
+    
+    NSDictionary<NSString*, NSNumber*> *productStats = [self.products valueForKey:self.selectedProductLabel.text];
+    
+    if (productStats == NULL) {
+        
+        UIAlertController* alertController = [UIAlertController
+                                              alertControllerWithTitle:@"Error"
+                                              message:@"Select a product first!"
+                                              preferredStyle:UIAlertControllerStyleAlert];
+
+        
+        UIAlertAction* defaultAction = [UIAlertAction
+                                        actionWithTitle:@"OK!"
+                                        style:UIAlertActionStyleDefault
+                                        handler:NULL];
+        
+        [alertController addAction:defaultAction];
+        
+        [self presentViewController:alertController animated:YES completion:NULL];
+        
+        return;
+    }
+    
+    NSString* country;
+    double price = 0;
+    
+    for (NSString* name in productStats) {
+        double tempPrice = [[productStats valueForKey:name] integerValue];
+        
+        if (tempPrice > price) {
+            country = name;
+            price = tempPrice;
+        }
+    }
+    
+    NSLog(@"Sample %@ %@: %.2f", self.selectedProductLabel.text, country, price);
+    
+    self.maxSalesLabel.text = [NSString stringWithFormat:@"%@: %.2f", country, price];
 }
 
 
